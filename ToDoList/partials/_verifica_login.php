ce0230b30b90fb98396b0f818d5bef9defb0c5d4
Icaro Peretti <icaroperetti50@gmail.com>
2020-10-10 23:09:39 -0300

    Update _verifica_login.php

--- ToDoList/partials/_verifica_login.php
+++ ToDoList/partials/_verifica_login.php
@@ -3,10 +3,9 @@
     require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php'); 
     use App\utils\FlashMessage;
 
-    if($_SESSION['logado'] == false){
+     if(isset($_SESSION['logado']) != true){
         FlashMessage::setMessage("Você precisa estar logado",FlashMessage::ERROR);
         header('location: /index.php');
         exit(0);
     }
-
 ?>
