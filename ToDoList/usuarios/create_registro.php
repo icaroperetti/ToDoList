f31376bcd0bc7271f8b95af301772536d3ad6b2c
Icaro Peretti <icaroperetti50@gmail.com>
2020-10-23 12:57:39 -0300

    Add files via upload

--- ToDoList/usuarios/create_registro.php
+++ ToDoList/usuarios/create_registro.php
@@ -8,12 +8,20 @@
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
+   
+
+    if(empty($nome) || empty($email)||empty($senha)){
+        FlashMessage::setMessage("Por favor, insira todos os dados para cadastro",FlashMessage::ERROR);
+        header("Location: /usuarios/registrar_front.php");
+        exit(0);
+    }
 
     $hashed_password = password_hash($senha, PASSWORD_DEFAULT);
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $stmtEmail = usuarioDAO::validation($email);
-        
+        $stmtEmail = usuarioDAO::validation(filter_var($email, FILTER_VALIDATE_EMAIL));
+
+
         if ($stmtEmail->rowCount() > 0) {
             FlashMessage::setMessage("Email já está sendo utilizado", FlashMessage::ERROR);
             header("Location: /usuarios/registrar_front.php");
