d5365fb54bb5b39e83961d7c53ea3cdba4519fa0
Icaro Peretti <icaroperetti50@gmail.com>
2021-07-01 13:43:26 -0300

    Update _head.php

--- ToDoList/partials/_head.php
+++ ToDoList/partials/_head.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="author" content="Ícaro Peretti Baseggio">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="sortcut icon" href="/img/prancheta.png" 
+<link rel="sortcut icon" href="/img/prancheta.png" >
 <link rel="stylesheet" href="/css/bootstrap-reboot.css" />
 <link rel="stylesheet" href="/css/bootstrap-grid.css" />
 <link rel="stylesheet" href="/css/bootstrap.css" />
